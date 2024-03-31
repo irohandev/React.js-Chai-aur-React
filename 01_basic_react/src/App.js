0edb788d0b0b420233ff2a51e86db48f5405014e
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-31 09:57:31 +0530

    Update

--- 01_basic_react/src/App.js
+++ 01_basic_react/src/App.js
@@ -1,8 +1,11 @@
-
+import Test1 from './Test1'
 
 function App() {
   return (
+    <>
     <h1>Starting with ReactJS</h1>
+    <Test1/>
+    </>
   );
 }
 
---
+++ 01_basic_react/src/Test1.js
@@ -0,0 +1,6 @@
+function Test1(){
+    return(
+        <h3>Heyyyyyy...Heyyyyyyyy!</h3>
+    )
+}
+export default Test1
