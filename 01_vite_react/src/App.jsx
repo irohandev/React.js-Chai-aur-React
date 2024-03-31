d004264ce3f3715965797ce8555a4f18e0e8f517
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-31 13:31:37 +0530

    Update

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,4 +1,6 @@
 import Test from "./Test"
+
+/*
 function App() {
 
   return (
@@ -7,11 +9,23 @@ function App() {
     <Test/>     
     </>
   )
+}*/
+
+function App() {
+  const name = "Rohan"
+
+  return (
+    <>
+    <h1>Hello {name}!</h1>    
+    </>
+  )
 }
+
 
 export default App
 
 // NOTE:
-// line 5 <> - isko fragment kehte hai !
+// line 5 - <> = isko fragment kehte hai !
 // line 5 - idhar bass ek hi tag return kr skte hai isliye ek hi tag mein in sbko close kr diye hai..iske andar bhot se element ho skte hai
 // line 7 - Test ko bhi idhr leke aa gaye hai
+// line 19 - {name} = aise react mein variable use kr skte hai f string jaise python mein bs yaha final output k bdd yeh use krna hai
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>    //yaha bhi likh skte hai react mein functiions ko!
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',      //yeh run nahi krega kyuki yeh mere custom react k part hai mere accrding!
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (  
+  <a href= "https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',                                                    //type; bss yeh react ke accrding aise bnta hai; lkin yaha pe type likhte nhi
+  {href: 'https://google.com', target: '_blank'},         //yeh hua objects with its properties ispe bhi direct bs {} parentheses se likhte
+  'Click me to visit google'                              //yeh hua children jo ki third part hai
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    //reactElement      //direct object ko aise likhnge toh aise yaha pe execute ho jana hai 
+    <App/>
 )
