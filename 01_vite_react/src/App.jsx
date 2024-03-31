c317469de57662c1ee5b3e363257662a13dcc30e
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-31 09:47:00 +0530

    Update

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,9 +1,17 @@
-
+import Test from "./Test"
 function App() {
 
   return (
+    <>          
     <h1>Starting with ReactJS with Vite</h1>
+    <Test/>     
+    </>
   )
 }
 
 export default App
+
+// NOTE:
+// line 5 <> - isko fragment kehte hai !
+// line 5 - idhar bass ek hi tag return kr skte hai isliye ek hi tag mein in sbko close kr diye hai..iske andar bhot se element ho skte hai
+// line 7 - Test ko bhi idhr leke aa gaye hai
---
+++ 01_vite_react/src/Test.jsx
@@ -0,0 +1,6 @@
+function Test(){
+    return(
+        <h3>Hello...Hello!</h3>
+    )
+}
+export default Test
