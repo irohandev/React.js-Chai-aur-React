ddfbc3a80f1271f98664e473789f9fbf249fc9be
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-03-31 09:07:42 +0530

    Update

---
+++ 01_basics/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Starting with ReactJS</h1>
+  );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Starting with ReactJS with Vite</h1>
+  )
+}
+
+export default App
