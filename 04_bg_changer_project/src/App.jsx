60438f1b6013e726336c535b523da221bed3a97e
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-05-07 21:40:05 +0530

    Update

--- 04_bg_changer_project/src/App.jsx
+++ 04_bg_changer_project/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <>
       <div className=" w-full h-screen duration-200"
         style = {{backgroundColor: color}}>
-          
         <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
           <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
             <button
